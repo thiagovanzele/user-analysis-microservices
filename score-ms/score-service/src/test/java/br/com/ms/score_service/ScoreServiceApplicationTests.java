369637282da711d87d3d0cff619a82758fcd20eb
Thiago Vanzele <thiagovanzele@gmail.com>
2026-03-29 23:20:00 -0300

    score ms

---
+++ score-ms/score-service/src/test/java/br/com/ms/score_service/ScoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ms.score_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
