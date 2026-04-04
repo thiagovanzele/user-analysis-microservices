90cc7e0fa12a8dc9db5c396ed354a2147f7e6a63
Thiago Vanzele <thiagovanzele@gmail.com>
2026-04-04 16:46:43 -0300

    feat: PageResponse generalista

---
+++ user-ms/user-service/src/main/java/br/com/ms/user_service/model/dto/response/PageResponse.java
@@ -0,0 +1,25 @@
+package br.com.ms.user_service.model.dto.response;
+
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public record PageResponse<T>(
+        List<T> content,
+        int page,
+        int size,
+        long totalElements,
+        int totalPages,
+        boolean last
+) {
+    public static <T> PageResponse<T> fromPage(Page<T> page) {
+        return new PageResponse<>(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isLast()
+        );
+    }
+}
