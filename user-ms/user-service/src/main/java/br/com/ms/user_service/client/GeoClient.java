69457b7d01b8bba1230310ed37225dccfc475019
Thiago Vanzele <thiagovanzele@gmail.com>
2026-04-05 17:15:34 -0300

    chore: melhoria na validação de UF

--- user-ms/user-service/src/main/java/br/com/ms/user_service/client/GeoClient.java
+++ user-ms/user-service/src/main/java/br/com/ms/user_service/client/GeoClient.java
@@ -1,6 +1,7 @@
 package br.com.ms.user_service.client;
 
 import br.com.ms.user_service.model.dto.response.EnderecoResponse;
+import br.com.ms.user_service.model.entities.Usuario;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- user-ms/user-service/src/main/java/br/com/ms/user_service/model/entities/Endereco.java
+++ user-ms/user-service/src/main/java/br/com/ms/user_service/model/entities/Endereco.java
@@ -1,11 +1,16 @@
 package br.com.ms.user_service.model.entities;
 
 import br.com.ms.user_service.model.dto.response.EnderecoResponse;
+import br.com.ms.user_service.model.enums.UF;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Optional;
 
 @Embeddable
 @Builder
@@ -18,7 +23,9 @@ public class Endereco {
     private String logradouro;
     private String bairro;
     private String localidade;
-    private String uf;
+
+    @Enumerated(EnumType.STRING)
+    private UF uf;
 
     public static Endereco of(EnderecoResponse response) {
 
@@ -26,12 +33,16 @@ public class Endereco {
             throw new IllegalArgumentException("Endereço não pode ser null");
         }
 
-        return builder()
+        Optional<UF> ufFromResponse = UF.fromApi(response.uf());
+
+        EnderecoBuilder enderecoBuilder = builder()
                 .bairro(response.bairro())
                 .cep(response.cep())
                 .logradouro(response.logradouro())
-                .localidade(response.localidade())
-                .uf(response.uf())
-                .build();
+                .localidade(response.localidade());
+
+        ufFromResponse.ifPresent(enderecoBuilder::uf);
+
+        return enderecoBuilder.build();
     }
 }
