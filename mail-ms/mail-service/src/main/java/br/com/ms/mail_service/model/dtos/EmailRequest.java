e2f9c07da9d6c1d9e2cae1aca1842cb837a8f563
Thiago Vanzele <thiagovanzele@gmail.com>
2026-04-05 18:05:54 -0300

    chore: remove import sem uso

--- mail-ms/mail-service/src/main/java/br/com/ms/mail_service/model/dtos/EmailRequest.java
+++ mail-ms/mail-service/src/main/java/br/com/ms/mail_service/model/dtos/EmailRequest.java
@@ -1,8 +1,4 @@
 package br.com.ms.mail_service.model.dtos;
 
-import tools.jackson.core.SerializableString;
-
-import java.util.List;
-
 public record EmailRequest(UsuarioRequest para, String assunto, String mensagem) {
 }
